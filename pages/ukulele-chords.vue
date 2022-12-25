757b8a0180caa9fa1c6b887156b026e8e45ff41e
Muhammad A Faishal <maafaishal@gmail.com>
2022-12-25 16:49:18 +0700

    chore: ukulele content

--- pages/ukulele-chords.vue
+++ pages/ukulele-chords.vue
@@ -1,8 +1,23 @@
 <script lang="ts" setup>
+import { CHORDS, SUFFIXES } from '@/constants/chords'
 </script>
 
 <template>
-  <div>
-    <h1>Ukulele</h1>
+  <div class="pt-12 pb-16">
+    <div class="text-center mb-12">
+      <h1 class="text-6xl">
+        Ukulele Chords
+      </h1>
+    </div>
+    <div class="w-full">
+      <div v-for="chord in CHORDS" :key="chord" class="w-full flex mb-6">
+        <div v-for="suffix in SUFFIXES" :key="suffix" class="flex-grow text-center">
+          <p>
+            {{ chord }}{{ suffix }}
+          </p>
+          <img :src="`/imgs/ukulele-chords/${chord.includes('#') ? chord.replace('#', '-sharp') : chord}/${suffix}.svg`" class="w-full">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
