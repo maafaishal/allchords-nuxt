5a879f51dcfef2c5617d0dc08cd6cc076bded474
Muhammad A Faishal <maafaishal@gmail.com>
2022-12-25 16:14:51 +0700

    refactor: guitar page content

---
+++ constants/chords.ts
@@ -0,0 +1,32 @@
+export const CHORDS = [
+  'C',
+  'Db',
+  'D',
+  'Eb',
+  'E',
+  'F',
+  'Gb',
+  'G',
+  'Ab',
+  'A',
+  'Bb',
+  'B'
+]
+
+export const CHORDS_V2 = [
+  'C',
+  'C#',
+  'D',
+  'D#',
+  'E',
+  'F',
+  'F#',
+  'G',
+  'G#',
+  'A',
+  'Bb',
+  'B'
+]
+
+export const SUFFIXES = ['major', '7', 'maj7', 'm', 'm7', 'aug']
+export const SUFFIXES_V2 = ['major', '7', 'maj7', 'm', 'm7']
--- pages/guitar-chords.vue
+++ pages/guitar-chords.vue
@@ -1,8 +1,21 @@
 <script lang="ts" setup>
+import { CHORDS_V2, SUFFIXES } from '@/constants/chords'
 </script>
 
 <template>
-  <div>
-    <h1>Guitar</h1>
+  <div class="pt-12 pb-16">
+    <div class="text-center mb-12">
+      <h1 class="text-6xl">
+        Guitar Chords
+      </h1>
+    </div>
+    <div class="w-full">
+      <div v-for="chord in CHORDS_V2" :key="chord" class="w-full flex mb-6">
+        <div v-for="suffix in SUFFIXES" :key="suffix" class="flex-grow text-center">
+          <p>{{ chord }}{{ suffix }}</p>
+          <NuxtImg :src="`/imgs/guitar-chords/${chord.includes('#') ? chord.replace('#', '-sharp') : chord}/${suffix}.svg`" class="w-full" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
