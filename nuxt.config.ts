318e0234d83fdf449076059a5709e5e0f233a1bf
Muhammad A Faishal <maafaishal@gmail.com>
2022-12-27 16:19:55 +0700

    style: rubik font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,14 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@nuxt/image-edge',
     'nuxt-icon'
-  ]
+  ],
+  // @ts-expect-error
+  buildModules: [
+    '@nuxtjs/google-fonts'
+  ],
+  googleFonts: {
+    families: {
+      Rubik: [300, 400, 500, 600, 700]
+    }
+  }
 })
