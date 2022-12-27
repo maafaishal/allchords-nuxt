3a58369eda2e629c748c57094b9945b822baf207
Muhammad A Faishal <maafaishal@gmail.com>
2022-12-27 17:01:39 +0700

    style: footer color

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="w-full bg-gray-300">
+  <footer class="w-full bg-gray-700 text-white">
     <div class="container py-4 mx-auto flex justify-between">
       <p>Ⓒ All-Chords</p>
       <p>Made with <span class="text-red-600">♥</span></p>
